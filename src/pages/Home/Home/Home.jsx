6588e5d89e7944d02ba5c2d3589d8012be592b59
sahalmahin <samirsatter0142@gmail.com>
2023-12-21 16:15:53 +0600

    Design Hero Section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,13 +1,14 @@
 import Footer from "../../Shared/Footer/Footer";
 import Banner from "../Banner/Banner";
 import Blogs from "../Blogs/Blogs";
+import NewsLetter from "../NewsLetter/NewsLetter";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Blogs></Blogs>
-            
+            <NewsLetter></NewsLetter>
             <Footer></Footer>
         </div>
     );
--- src/pages/Home/NewsLetter/NewsLetter.jsx
+++ src/pages/Home/NewsLetter/NewsLetter.jsx
@@ -1,7 +1,25 @@
 const NewsLetter = () => {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        console.log(email);
+    }
+
     return (
-        <div>
-            
+        <div className="my-60">
+            <h3 className="text-5xl font-bold text-center">News Letter</h3>
+            <div className="flex items-center justify-center mt-16">
+                <form onSubmit={handleSubmit}>
+                    <div className="form-control">
+                        <input type="email" name='email' placeholder="Type your Email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control mt-6">
+                        <input className="btn btn-primary" type="submit" />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
